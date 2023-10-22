f80d2c4f1b8afa4cd67f66ab9a0efd9357a6e629
moontacer <montasserthegangsta@gmail.com>
2023-10-22 16:33:19 +0100

    add Kafka streaming

--- dags/kafka-stream.py
+++ dags/kafka-stream.py
@@ -35,9 +35,21 @@ def format_data(res):
 
 def stream_data():
     import json
-    res = get_data()
-    res = format_data(res)
-    print(json.dumps(res, indent=3))
+    from kafka import KafkaProducer
+    import time
+    import logging
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
+    curr_time = time.time()
+    while True:
+        if time.time() > curr_time + 60: #1 minute
+            break
+        try:
+            res = get_data()
+            res = format_data(res)
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
+        except Exception as e:
+            logging.error(f'An error occured: {e}')
+            continue
 
 with DAG('user_automation',
         default_args = default_args,

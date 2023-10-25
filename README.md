fd7329f3a7f843a87bb9b5f515c0baeca5864ea3
moontacer <montasserthegangsta@gmail.com>
2023-10-25 02:16:14 +0100

    add readme, architecture, refactor...

---
+++ README.md
@@ -0,0 +1,16 @@
+## Architecture
+
+![1698196520888](image/README/1698196520888.png)
+
+## Description
+
+I used Airflow, PostgreSQL, Kafka, Spark and Cassandra in order to establish a fully automated ETL pipeline in a container runtime, with a CI using GitHub Actions to automate the service's Docker image updates on DockerHub.
+
+## Get Started
+
+- Clone the repository
+  - `git clone https://github.com/moontucer/Data-Streaming-Project/`
+- Go to the project folder
+  - `cd Data-Streaming-Project`
+- Build the environment with Docker Compose
+  - `docker-compose up`
--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,24 +1,26 @@
-#import uuid
+import uuid
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 default_args = {
-    'owner': 'mountasser',
-    'start_date': datetime(2023, 8, 3, 10, 00)
+    'owner': 'airscholar',
+    'start_date': datetime(2023, 9, 3, 10, 00)
 }
 
 def get_data():
     import requests
+
     res = requests.get("https://randomuser.me/api/")
     res = res.json()
     res = res['results'][0]
+
     return res
 
 def format_data(res):
     data = {}
     location = res['location']
-    #data['id'] = uuid.uuid4()
+    data['id'] = uuid.uuid4()
     data['first_name'] = res['name']['first']
     data['last_name'] = res['name']['last']
     data['gender'] = res['gender']
@@ -31,6 +33,7 @@ def format_data(res):
     data['registered_date'] = res['registered']['date']
     data['phone'] = res['phone']
     data['picture'] = res['picture']['medium']
+
     return data
 
 def stream_data():
@@ -38,27 +41,28 @@ def stream_data():
     from kafka import KafkaProducer
     import time
     import logging
+
     producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
     curr_time = time.time()
+
     while True:
         if time.time() > curr_time + 60: #1 minute
             break
         try:
             res = get_data()
             res = format_data(res)
+
             producer.send('users_created', json.dumps(res).encode('utf-8'))
         except Exception as e:
             logging.error(f'An error occured: {e}')
             continue
 
 with DAG('user_automation',
-        default_args = default_args,
-        schedule_interval = '@daily',
-        catchup = False) as dag:
+         default_args=default_args,
+         schedule_interval='@daily',
+         catchup=False) as dag:
 
     streaming_task = PythonOperator(
-        task_id = 'stream_data_from_api',
-        python_callable = stream_data
+        task_id='stream_data_from_api',
+        python_callable=stream_data
     )
-
-stream_data()
